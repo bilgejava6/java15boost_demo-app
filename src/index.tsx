2606400fe5bc9c3a962493ec41951ed64b6c2a57
bilgejava6 <bilgejava6@gmail.com>
2024-11-08 15:47:19 +0300

    ödev2

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import Login from './pages/Login';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Odev from './pages/Odev';
+import Odev2 from './pages/Odev2';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Odev />
+  <Odev2 />
 );
---
+++ src/pages/Odev2.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+function Odev2() {
+
+    const [userName,setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [rePassword, setRePassword] = useState('');
+    const [age, setAge] = useState(0);
+    const [isWrong, setIsWrong] = useState(false);
+
+  return (
+    <div className="contanier">
+        <div className="row mt-4">
+            <div className="col-3"></div>
+            <div className="col-4">
+                <h1 className='text-center mb-3 text-success'>Register Form</h1>
+                <div className="mb-3">
+                    <input onChange={evt=>{setUserName(evt.target.value)}} className='form-control' type="text" placeholder='kullanıcı adı' />
+                </div>
+                <div className="mb-3">
+                    <input onChange={evt=>{setPassword(evt.target.value)}} className='form-control' type="password" placeholder='şifre'/>
+                </div>
+                <div className="mb-3">
+                    <input onChange={evt=>{
+                        setRePassword(evt.target.value);
+                        // DİKKAT!!!!
+                        // set işlemi bir dizi fonksiyon tetiklediği için zaman alır bu nedenle
+                        // ilgili deği hemen aktif etmez yani set ettiğiniz değer altta hemen kullanılamaz
+                        if(evt.target.value === password) setIsWrong(false);
+                        else setIsWrong(true);
+                       // setIsWrong(!(evt.target.value === password)) // eğer şifreler uyuşmuyor ise TRUE
+                        }} className='form-control' type="password" placeholder='şifre doğrula' />
+                    {
+                        isWrong ? <label className='text-danger'>* şifreler uyuşmuyor</label> : null
+                    }
+                    
+                </div>
+                <div className="mb-3">
+                    <input onChange={evt=>{setAge(parseInt(evt.target.value))}} className='form-control' type="number" placeholder='yaş'/>
+                </div>
+                <div className="mb-3 d-grid">
+                    <button className='btn btn-secondary'>Yeni Üye</button>
+                </div>
+
+            </div>
+            <div className="col-5"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Odev2
