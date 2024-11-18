5a98b7bcb5af85f6cf1c402b4bf3a16af8749b55
bilgejava6 <bilgejava6@gmail.com>
2024-11-18 09:55:37 +0300

    navigate

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,48 +1,60 @@
 import React from "react";
-
+import {
+    useNavigate
+} from 'react-router-dom'
 function Home() {
+    const navigate = useNavigate();
+    const openPage = (page: string)=>{
+        navigate(page);
+    }
 	return (
 		<div className="contanier">
-			<div className="row">
+			<div className="row mt-5 ">
 				<div className="col-3"></div>
-				<div className="col-6 border">
-					<div className="mb-3">
+				<div className="col-6 border p-3 ">
+					<div className="mb-3 d-grid">
 						<input
+                            onClick={()=>openPage('/login')}
 							type="button"
 							value="Login Page"
 							className="btn btn-outline-success"
 						/>
 					</div>
-					<div className="mb-3">
+					<div className="mb-3 d-grid">
 						<input
+                        onClick={()=>openPage('/product')}
 							type="button"
 							value="Porduct Page"
 							className="btn btn-outline-success"
 						/>
 					</div>
-					<div className="mb-3">
+					<div className="mb-3 d-grid">
 						<input
+                        onClick={()=>openPage('/avatar')}
 							type="button"
 							value="Avatar Page"
 							className="btn btn-outline-success"
 						/>
 					</div>
-					<div className="mb-3">
+					<div className="mb-3 d-grid">
 						<input
+                        onClick={()=>openPage('/user')}
 							type="button"
 							value="User Page"
 							className="btn btn-outline-success"
 						/>
 					</div>
-					<div className="mb-3">
+					<div className="mb-3 d-grid">
 						<input
+                        onClick={()=>openPage('/yarisma')}
 							type="button"
 							value="Yarışma Page"
 							className="btn btn-outline-success"
 						/>
 					</div>
-					<div className="mb-3">
+					<div className="mb-3 d-grid">
 						<input
+                        onClick={()=>openPage('/user-list')}
 							type="button"
 							value="User List Page"
 							className="btn btn-outline-success"
